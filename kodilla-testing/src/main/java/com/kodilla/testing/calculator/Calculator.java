185a7277513e330006f0aff3add4e0bbd2f09f29
annafiega <annafiega@gmail.com>
2018-01-10 08:36:44 +0100

    calculator test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+
+}
+
