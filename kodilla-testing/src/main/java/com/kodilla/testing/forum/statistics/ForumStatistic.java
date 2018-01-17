689894148db8f8b283aa8991366882367ea77006
annafiega <annafiega@gmail.com>
2018-01-17 20:12:11 +0100

    Forum ststistic - wersja1

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistic.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistic.java
@@ -12,33 +12,29 @@ public class ForumStatistic {
     private double avgNubmerOfCommentsPerPost;
     Statistics statistics;
 
-   /* public ForumStatistic(Statistics statistics){
-        this.statistics=statistics;
-    }*/
 
-    public void calculateAdvStatistics(Statistics statistics){
-        this.numberOfUsers=statistics.usersNames().size();
-        this.numberOfPosts=statistics.postsCount();
-        this.numberOfComments=statistics.commentsCount();
 
-        if (numberOfPosts==0 ||numberOfUsers==0){
-            this.avgNumberOfPostsPerUser=0;
-            this.avgNumberOfCommentsPerUser=0;
-            this.avgNubmerOfCommentsPerPost=0;
+    public void calculateAdvStatistics(Statistics statistics) {
+        this.numberOfUsers = statistics.usersNames().size();
+        this.numberOfPosts = statistics.postsCount();
+        this.numberOfComments = statistics.commentsCount();
+
+        if (numberOfPosts == 0 || numberOfUsers == 0) {
+            this.avgNumberOfPostsPerUser = 0;
+            this.avgNumberOfCommentsPerUser = 0;
+            this.avgNubmerOfCommentsPerPost = 0;
+            this.numberOfPosts=0;
+            this.numberOfComments=0;
+            }else{
+            this.avgNumberOfPostsPerUser = this.numberOfPosts / numberOfUsers;
+            this.avgNumberOfCommentsPerUser = this.numberOfComments / numberOfUsers;
+            this.avgNubmerOfCommentsPerPost = this.numberOfComments / numberOfPosts;
         }
-
-
-        this.avgNumberOfPostsPerUser=this.numberOfPosts/this.numberOfUsers;
-        this.avgNumberOfCommentsPerUser=this.numberOfComments/this.numberOfUsers;
-        this.avgNubmerOfCommentsPerPost=this.numberOfComments/ this.numberOfPosts;
-
-
-
-
     }
-    public String  ShowStatistics(){
+    public String  showStatistics(){
         String result;
-        result=("Number of Users: "+ this.numberOfUsers +"/n"+ "Number of posts: "+ this.numberOfPosts +"/n"+ "Number of comments: "+ this.numberOfComments +"/n"+ "Average number of posts per user: " + this.avgNumberOfPostsPerUser+"/n"+"Average number of comments per user: "+this.avgNumberOfCommentsPerUser +"/n"+"Average number of comments per post: "+this.avgNubmerOfCommentsPerPost);
+        result=("Number of Users: "+ this.numberOfUsers +"\n"+ "Number of posts: "+ this.numberOfPosts +"\n"+ "Number of comments: "+ this.numberOfComments +"\n"+ "Average number of posts per user: " + this.avgNumberOfPostsPerUser+"\n"+"Average number of comments per user: "+this.avgNumberOfCommentsPerUser +"\n"+"Average number of comments per post: "+this.avgNubmerOfCommentsPerPost+"\n");
+        { System.out.println (result);}
         return result;
     }
 }
