bcc388f86be349db2362367155c7d7649ddf7d98
annafiega <annafiega@gmail.com>
2018-02-15 14:35:05 +0100

    11.1

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.singleton;
+
+public class Logger {
+    private static Logger loggerInstance =null;
+    private String lastLog = "";
+
+    public static Logger getInstance(){
+        if (loggerInstance==null){
+            loggerInstance=new Logger();
+        }
+        return loggerInstance;
+    }
+
+    public void log(String log) {
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
+}
