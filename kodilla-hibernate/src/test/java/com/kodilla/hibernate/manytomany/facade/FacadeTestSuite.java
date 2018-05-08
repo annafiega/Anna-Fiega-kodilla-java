69f3d11596914776a07b80258f4303f5c14fcff2
annafiega <annafiega@gmail.com>
2018-05-08 10:33:10 +0200

    20.1

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
@@ -24,23 +24,11 @@ public class FacadeTestSuite {
     @Autowired
     private Facade facade;
 
-    @Autowired
-    private CompanyDao companyDao;
-
-    @Autowired
-    private EmployeeDao employeeDao;
-
     @Test
 
     public void testSearchCompanyByName(){
 
         final String prefix="Sof";
-        List<Company> companyList=new ArrayList<>();
-        companyList.add(new Company("Software Machine"));
-        companyList.add(new Company("Data Maesters"));
-        companyList.add( new Company("Grey Matter"));
-        companyList.add(new Company("Sofy.pl"));
-        companyList.add(new Company("Softshell"));
 
         try{
             List <Company>resultList=facade.searchCompanyByName(prefix);
@@ -52,11 +40,6 @@ public class FacadeTestSuite {
 
     public void testSearchEmployeeByName(){
         final String prefix="Smi";
-        List<Employee> employeeList=new ArrayList<>();
-        employeeList.add(new Employee ("John", "Smith"));
-        employeeList.add(new Employee("Stephanie", "Clarckson"));
-        employeeList.add(new Employee("Linda", "Kovalsky"));
-        employeeList.add(new Employee("Amanda", "Kovalsky"));
 
         try{
             List <Employee>resultList=facade.searchEmployeeByName(prefix);
